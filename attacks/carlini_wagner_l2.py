35372b477c5b3681f19cc5574c33a3f7cfacaf15
Elre <vze.ezv@gmail.com>
2020-05-26 15:21:39 +0100

    Modified to support specifying attack parameters via yaml config

--- attacks/carlini_wagner_l2.py
+++ attacks/carlini_wagner_l2.py
@@ -17,7 +17,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
-from more_itertools import one
+from attacks.attack_utils import convert_labels
 
 
 def one_hot_embedding(labels, num_classes):
@@ -33,6 +33,7 @@ def one_hot_embedding(labels, num_classes):
     y = torch.eye(num_classes)
     return y[labels]
 
+
 def atanh(x, eps=1e-6):
     """
     The inverse hyperbolic tangent function, missing in pytorch.
@@ -43,6 +44,7 @@ def atanh(x, eps=1e-6):
     """
     x = x * (1 - eps)
     return 0.5 * torch.log((1.0 + x) / (1.0 - x))
+
 
 def to_tanh_space(x, box):
     """
@@ -57,6 +59,7 @@ def to_tanh_space(x, box):
     _box_mul = (box[1] - box[0]) * 0.5
     _box_plus = (box[1] + box[0]) * 0.5
     return atanh((x - _box_plus) / _box_mul)
+
 
 def from_tanh_space(x, box):
     """
@@ -74,7 +77,7 @@ def from_tanh_space(x, box):
     return torch.tanh(x) * _box_mul + _box_plus
 
 
-class L2Adversary(object):
+class CarliniWagnerL2(object):
     """
     The L2 attack adversary. To enforce the box constraint, the
     change-of-variable trick using tanh-space is adopted.
@@ -135,9 +138,21 @@ class L2Adversary(object):
     - ``M``: the number of classes
     """
 
-    def __init__(self, targeted=False, confidence=0.0, c_range=(1e-3, 1e10),
-                 search_steps=10, max_steps=100, abort_early=True,
-                 box=(-1., 1.), optimizer_lr=1e-2, init_rand=False):
+    def __init__(self, targeted=False,
+                 confidence=0.0,
+                 c_lower=1e-3,
+                 c_upper=1e10,
+                 binary_search_steps=10,
+                 max_iterations=100,
+                 abort_early=True,
+                 box_lower=-1.,
+                 box_upper=1.,
+                 optimizer_lr=1e-2,
+                 init_rand=False,
+                 attack_mode='context',
+                 class_fraction=0.4,
+                 shot_fraction=0.4
+                 ):
         """
         :param targeted: ``True`` to perform targeted attack in ``self.run``
                method
@@ -152,9 +167,9 @@ class L2Adversary(object):
         :param search_steps: the number of steps to perform binary search of
                the constant :math:`c` over ``c_range``
         :type search_steps: int
-        :param max_steps: the maximum number of optimization steps for each
+        :param max_iterations: the maximum number of optimization steps for each
                constant :math:`c`
-        :type max_steps: int
+        :type max_iterations: int
         :param abort_early: ``True`` to abort early in process of searching for
                :math:`c` when the loss virtually stops increasing
         :type abort_early: bool
@@ -185,33 +200,29 @@ class L2Adversary(object):
         (https://github.com/rwightman/pytorch-nips2017-attack-example.git),
         though, the learning rate is set to 5e-4.
         """
-        if len(c_range) != 2:
-            raise TypeError('c_range ({}) should be of form '
-                            'tuple([lower_bound, upper_bound])'
-                            .format(c_range))
-        if c_range[0] >= c_range[1]:
+        if c_lower >= c_upper:
             raise ValueError('c_range lower bound ({}) is expected to be less '
-                             'than c_range upper bound ({})'.format(*c_range))
-        if len(box) != 2:
-            raise TypeError('box ({}) should be of form '
-                            'tuple([lower_bound, upper_bound])'
-                            .format(box))
-        if box[0] >= box[1]:
+                             'than c_range upper bound ({})'.format(c_lower, c_upper))
+        if box_lower >= box_upper:
             raise ValueError('box lower bound ({}) is expected to be less than '
-                             'box upper bound ({})'.format(*box))
+                             'box upper bound ({})'.format(box_lower, box_upper))
         self.targeted = targeted
         self.confidence = float(confidence)
-        self.c_range = (float(c_range[0]), float(c_range[1]))
-        self.binary_search_steps = search_steps
-        self.max_steps = max_steps
+        self.c_range = (float(c_lower), float(c_upper))
+        self.binary_search_steps = binary_search_steps
+        self.max_iterations = max_iterations
         self.abort_early = abort_early
         self.ae_tol = 1e-4  # tolerance of early abort
-        self.box = tuple(map(float, box))  # type: Tuple[float, float]
+        self.box = (float(box_lower), float(box_upper))
         self.optimizer_lr = optimizer_lr
 
         # `self.init_rand` is not in Carlini's code, it's an attempt in the
         # referencing pytorch implementation to improve the quality of attacks.
         self.init_rand = init_rand
+
+        self.attack_mode = attack_mode
+        self.class_fraction = class_fraction
+        self.shot_fraction = shot_fraction
 
         # Since the larger the `scale_const` is, the more likely a successful
         # attack can be found, `self.repeat` guarantees at least attempt the
@@ -224,25 +235,6 @@ class L2Adversary(object):
     def generate(self, context_images, context_labels, target_images, model, target_labels=None):
         """
         Produce adversarial examples for ``inputs``.
-
-        :param model: the model to attack
-        :type model: nn.Module
-        :param inputs: the original images tensor, of dimension [B x C x H x W].
-               ``inputs`` can be on either CPU or GPU, but it will eventually be
-               moved to the same device as the one the parameters of ``model``
-               reside
-        :type inputs: torch.FloatTensor
-        :param targets: the original image labels, or the attack targets, of
-               dimension [B]. If ``self.targeted`` is ``True``, then ``targets``
-               is treated as the attack targets, otherwise the labels.
-               ``targets`` can be on either CPU or GPU, but it will eventually
-               be moved to the same device as the one the parameters of
-               ``model`` reside
-        :type targets: torch.LongTensor
-        :param to_numpy: True to return an `np.ndarray`, otherwise,
-               `torch.FloatTensor`
-        :type to_numpy: bool
-        :return: the adversarial examples on CPU, of dimension [B x C x H x W]
         """
         ## TODO:  sanity check
         assert len(context_images.size()) == 4
@@ -252,11 +244,7 @@ class L2Adversary(object):
         else:
             ## Generate target labels based on model's initial prediction:
             initial_logits = model(context_images, context_labels, target_images)
-            target_labels = None #argmax something something on logits
-
-        # get a copy of targets in numpy before moving to GPU, used when doing
-        # the binary search on `scale_const`
-        target_labels_np = target_labels.clone().cpu().numpy()  # type: np.ndarray
+            target_labels = convert_labels(initial_logits[0])
 
         ## Get the ``way'' from the model
         num_classes = model.way
@@ -280,7 +268,7 @@ class L2Adversary(object):
         #    perturbations with the least L2 norms
         # - `o_best_advx`: the underlying adversarial example of `o_best_l2_ppred`
 
-        o_best_l2 = torch.ones(input_size) * 1e4 #placeholder for inf
+        o_best_l2 = torch.ones(input_size) * 1e4  # placeholder for inf
         o_best_l2_ppred = -torch.ones(input_size)
         o_best_advx = context_images.clone()
 
@@ -290,14 +278,15 @@ class L2Adversary(object):
         ## Hmm, why do these not require gradient?
         ## At any rate, Variable is deprecated and we can just turn the gradients off on the tensor directly instead
         context_images_tanh.requires_grad = False
-        #inputs_tanh_var = Variable(inputs_tanh, requires_grad=False)
+        # inputs_tanh_var = Variable(inputs_tanh, requires_grad=False)
 
         ## Make one-hot encoding for target_labels
         # the one-hot encoding of `target_labels`
         targets_labels_oh = one_hot_embedding(target_labels, num_classes)
         ## Should be able to remove stuff below, just leave it in to double check when stepping through that we get the same thing.
         ## Should be the same though
-        import pdb; pdb.set_trace()
+        import pdb;
+        pdb.set_trace()
         targets_labels_oh2 = torch.zeros(target_labels.size() + (num_classes,))  # type: torch.FloatTensor
         targets_labels_oh2.scatter_(1, target_labels.unsqueeze(1), 1.0)
         targets_labels_oh2 = Variable(targets_labels_oh2, requires_grad=False)
@@ -314,35 +303,37 @@ class L2Adversary(object):
         for sstep in range(self.binary_search_steps):
             if self.repeat and sstep == self.binary_search_steps - 1:
                 scale_consts_np = upper_bounds_np
-            scale_consts = torch.from_numpy(np.copy(scale_consts_np)).float().to(model.device)  # type: torch.FloatTensor
+            scale_consts = torch.from_numpy(np.copy(scale_consts_np)).float().to(
+                model.device)  # type: torch.FloatTensor
             # TODO: Check scale_consts doesn't require grad
-            import pdb; pdb.set_trace()
-            print('Using scale consts:', list(scale_consts_np) ) # FIXME
+            import pdb;
+            pdb.set_trace()
+            print('Using scale consts:', list(scale_consts_np))
 
             # the minimum L2 norms of perturbations found during optimization
-            best_l2 = torch.ones(input_size) * 1e4 #As placeholder for np.inf
+            best_l2 = torch.ones(input_size) * 1e4  # As placeholder for np.inf
             # the perturbed predictions corresponding to `best_l2`, to be used
             # in binary search of `scale_const`
             best_l2_ppred = -torch.ones(input_size)
             # previous (summed) batch loss, to be used in early stopping policy
-            prev_batch_loss = 1e4  #  as placeholder for infinity, type: float
+            prev_batch_loss = 1e4  # as placeholder for infinity, type: float
 
-            for optim_step in range(self.max_steps):
+            for optim_step in range(self.max_iterations):
 
                 # The adversarial examples in the image space
                 # of dimension [B x C x H x W]
                 adv_context_candidates = self._from_tanh_space(context_images_tanh + pert_tanh)
-                #TODO: It shouldn't be necessary to map the originals to tanh space and back,
+                # TODO: It shouldn't be necessary to map the originals to tanh space and back,
                 # #since we're working with -1,1 normalized images anyway
                 # the original inputs
-                #context_images = self._from_tanh_space(inputs_tanh_var)
+                # context_images = self._from_tanh_space(inputs_tanh_var)
 
                 batch_loss, pert_norms, pert_outputs, adv_context_images = \
                     self._optimize(adv_context_candidates, context_images, context_labels, target_images,
-                                   targets_labels_oh, scale_consts,  model, optimizer)
-                if optim_step % 10 == 0: print 'batch [{}] loss: {}'.format(optim_step, batch_loss)  # FIXME
+                                   targets_labels_oh, scale_consts, model, optimizer)
+                if optim_step % 10 == 0: print('batch [{}] loss: {}'.format(optim_step, batch_loss))
 
-                if self.abort_early and not optim_step % (self.max_steps // 10):
+                if self.abort_early and not optim_step % (self.max_iterations // 10):
                     if batch_loss > prev_batch_loss * (1 - self.ae_tol):
                         break
                     prev_batch_loss = batch_loss
@@ -350,9 +341,9 @@ class L2Adversary(object):
                 # update best attack found during optimization
                 pert_predictions = torch.argmax(pert_outputs, dim=1)
                 comp_pert_predictions = torch.argmax(
-                        self._compensate_confidence(pert_outputs,
-                                                    target_labels),
-                        dim=1)
+                    self._compensate_confidence(pert_outputs,
+                                                target_labels),
+                    dim=1)
 
                 for i in range(input_size):
                     l2 = pert_norms[i]
@@ -396,13 +387,13 @@ class L2Adversary(object):
                     else:
                         scale_consts_np[i] *= 10
 
-
         return o_best_advx
 
-#    def _optimize(self, model, optimizer, inputs_tanh_var, pert_tanh_var,
-#                 targets_oh_var, c_var):
-    #Instead of passing in the tanh context_images and perturbation, pass in the adversarial image and the originals
-    def _optimize(self, adv_context_images, context_images, context_labels, target_images, target_labels_oh, c, model, optimizer):
+    #    def _optimize(self, model, optimizer, inputs_tanh_var, pert_tanh_var,
+    #                 targets_oh_var, c_var):
+    # Instead of passing in the tanh context_images and perturbation, pass in the adversarial image and the originals
+    def _optimize(self, adv_context_images, context_images, context_labels, target_images, target_labels_oh, c, model,
+                  optimizer):
         """
         Optimize for one step.
 
@@ -430,14 +421,14 @@ class L2Adversary(object):
 
         perts_norm = torch.pow(adv_context_images - context_images, 2)
         perts_norm = torch.sum(perts_norm.view(
-                perts_norm.size(0), -1), 1)
+            perts_norm.size(0), -1), 1)
 
         ## target_active is a tensor of dimension [B], where the i-th entry is the logit value
         ## of the correct/target class for the i-th image
         # In Carlini's code, `target_active` is called `real`.
         target_active = torch.sum(target_labels_oh * pert_outputs, 1)
         inf = 1e4  # sadly pytorch does not work with np.inf;
-                   # 1e4 is also used in Carlini's code
+        # 1e4 is also used in Carlini's code
 
         ## Get the next most likely class i.e. for each image, find the maximum logit value
         ## that isn't the correct/target class's logit
@@ -453,7 +444,7 @@ class L2Adversary(object):
         assert (pert_outputs.max(1)[0] >= -inf).all(), 'assumption failed'
         # noinspection PyArgumentList
         max_other_active = torch.max(((1.0 - target_labels_oh) * pert_outputs
-                                        - target_labels_oh * inf), 1)[0]
+                                      - target_labels_oh * inf), 1)[0]
 
         # Compute $f(x')$, where $x'$ is the adversarial example in image space.
         # The result `f_eval` should be of dimension [B]
@@ -461,13 +452,13 @@ class L2Adversary(object):
             # if targeted, optimize to make `target_active` larger than
             # `max_other_active` by `self.confidence`
             f_eval = torch.clamp(max_other_active - target_active
-                                + self.confidence, min=0.0)
+                                 + self.confidence, min=0.0)
         else:
             # if not targeted, optimize to make `max_other_active` larger than
             # `target_active` (the ground truth image labels) by
             # `self.confidence`
             f_eval = torch.clamp(target_active - max_other_active
-                                + self.confidence, min=0.0)
+                                 + self.confidence, min=0.0)
         # the total loss of current batch, should be of dimension [1]
         combined_loss = torch.sum(perts_norm + c * f_eval)
 
@@ -495,7 +486,6 @@ class L2Adversary(object):
             return prediction == target
         else:
             return prediction != target
-
 
     def _compensate_confidence(self, outputs, targets):
         """
