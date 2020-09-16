1868cfd501defa86fd0315f471ef8c08acf26e5f
John Bronskill <jfb54@cam.ac.uk>
2020-09-16 19:25:17 +0100

    Added small scale basic plot.

---
+++ results/plot_bar_charts.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_labels = ['ProtoNets', 'MAML']
+legend_labels = ['PGD Specific', 'PGD General', 'UAP', 'Noise']
+colors = ['red', 'blue', 'green', 'black']
+
+
+def autolabel(ax, rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom', fontsize='large')
+
+
+def plot(ax, file, title):
+    data = np.genfromtxt(file, delimiter=',', skip_header=1, usecols=(1, 2))
+
+    x = np.arange(len(x_labels))  # the label locations
+    width = 0.22  # the width of the bars
+
+    rects1 = ax.bar(x - width * 3 / 2, data[3], width, label=legend_labels[3])
+    rects2 = ax.bar(x - width / 2, data[2], width, label=legend_labels[2])
+    rects3 = ax.bar(x + width / 2, data[0], width, label=legend_labels[0])
+    rects4 = ax.bar(x + width * 3 / 2, data[1], width, label=legend_labels[1])
+
+    right_side = ax.spines["right"]
+    right_side.set_visible(False)
+    top = ax.spines["top"]
+    top.set_visible(False)
+
+    ax.set_title(title, y=-0.25, fontsize='x-large')
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels, fontsize='x-large')
+
+    autolabel(ax, rects1)
+    autolabel(ax, rects2)
+    autolabel(ax, rects3)
+    autolabel(ax, rects4)
+
+
+def main():
+    files = [
+        './data/basic_target_epsilon-05.txt',
+        './data/basic_support_epsilon-05.txt',
+        './data/basic_target_epsilon-10.txt',
+        './data/basic_support_epsilon-10.txt'
+    ]
+
+    titles = [
+        'Query Attack (epsilon = 0.05)',
+        'Support Attack (epsilon = 0.05)',
+        'Query Attack (epsilon = 0.1)',
+        'Support Attack (epsilon = 0.1)'
+    ]
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, sharey=True, figsize=(20,5))
+    fig.set_dpi(100)
+
+    for file, title, ax in zip(files, titles, [ax1, ax2, ax3, ax4]):
+        plot(ax, file, title)
+
+    ax1.legend(ncol=len(legend_labels), bbox_to_anchor=(0, 1), loc='lower left', fontsize='x-large')
+
+    fig.text(-0.005, 0.5, 'Decrease in Accuracy (%)', va='center', rotation='vertical', fontsize='x-large')
+    fig.tight_layout()
+
+    plt.subplots_adjust(wspace=0.1)
+    plt.savefig('./plots/basic.pdf', bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    main()
