87516ded650677e0e2d615ab8ff971cd6fcb9407
John Bronskill <jfb54@cam.ac.uk>
2020-09-26 21:51:51 +0100

    Updated heat maps.

--- results/plot_heat_maps.py
+++ results/plot_heat_maps.py
@@ -5,7 +5,7 @@ from scipy.interpolate import griddata
 
 
 def heatmap(data, row_labels, col_labels, ax=None,
-            cbar_kw={}, cbarlabel="", **kwargs):
+            cbar_kw={}, cbarlabel="", show_xticks=True, **kwargs):
     """
     Create a heatmap from a numpy array and two lists of labels.
 
@@ -39,25 +39,29 @@ def heatmap(data, row_labels, col_labels, ax=None,
     # cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
 
     # We want to show all ticks...
-    ax.set_xticks(np.arange(data.shape[1]))
+    if show_xticks:
+        ax.set_xticks(np.arange(data.shape[1]))
     ax.set_yticks(np.arange(data.shape[0]))
     # ... and label them with the respective list entries.
-    ax.set_xticklabels(col_labels, fontsize='x-large')
+    if show_xticks:
+        ax.set_xticklabels(col_labels, fontsize='x-large')
     ax.set_yticklabels(row_labels, fontsize='x-large')
 
     # Let the horizontal axes labeling appear on top.
-    ax.tick_params(top=True, bottom=False,
-                   labeltop=True, labelbottom=False)
+    if show_xticks:
+        ax.tick_params(top=True, bottom=False,
+                       labeltop=True, labelbottom=False)
 
     # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=0, ha="center",
-             rotation_mode="anchor")
+    if show_xticks:
+        plt.setp(ax.get_xticklabels(), rotation=0, ha="center", rotation_mode="anchor")
 
     # Turn spines off and create white grid.
     for edge, spine in ax.spines.items():
         spine.set_visible(False)
 
-    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    if show_xticks:
+        ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
     ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
     ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
     ax.tick_params(which="minor", bottom=False, left=False)
@@ -125,40 +129,72 @@ def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
 
 
 def main():
-    fig, axs = plt.subplots(1, 4, sharey=True, sharex=True, figsize=(12,6))
+    fig, axs = plt.subplots(1, 2, sharey=True, sharex=True, figsize=(6,3))
 
     class_labels = ["1", "3", "5"]
     shot_labels = ["1", "3", "5"]
 
     files = [
         './data/scale_protonets_5-way_5-shot_epsilon-05.txt',
-        './data/scale_cnaps_5-way_5-shot_epsilon-05.txt',
-        './data/scale_protonets_5-way_5-shot_epsilon-10.txt',
-        './data/scale_cnaps_5-way_5-shot_epsilon-10.txt'
+        './data/scale_maml_5-way_5-shot_epsilon-05.txt'
     ]
 
     titles = [
-        '(a) ProtoNets,  epsilon: 0.05',
-        '(b) CNAPs, epsilon: 0.05',
-        '(c) ProtoNets, epsilon: 0.1',
-        '(d) CNAPs, epsilon: 0.1'
+        '(c) ProtoNets, 5-shot (Clean 66.3%)',
+        '(d) MAML, 5-shot (Clean 61.4%)'
     ]
 
     images = []
-    for file, title, ax in zip(files, titles, [axs[0], axs[1], axs[2], axs[3]]):
+    for file, title, ax in zip(files, titles, [axs[0], axs[1]]):
         data = np.genfromtxt(file, delimiter=',')
 
-        im = heatmap(data, class_labels, shot_labels, ax=ax, cmap="Reds", cbarlabel="% Drop in Accuracy")
+        im = heatmap(data, shot_labels, class_labels, ax=ax, cmap="Reds", cbarlabel="% Drop in Accuracy")
         images.append(im)
         texts = annotate_heatmap(im, valfmt="{x:.1f}", fontsize='x-large')
-        ax.set_title(title, y=-0.15, fontsize='x-large', color='blue')
+        ax.set_title(title, y=-0.15, fontsize='large', color='blue')
         ax.set_xlabel('Classes', fontsize='large')
         ax.xaxis.set_label_position('top')
 
     axs[0].set_ylabel('Shots', fontsize='large')
     plt.subplots_adjust(wspace=0.1)
     fig.tight_layout()
-    plt.savefig('./plots/heat_maps.pdf', bbox_inches='tight')
+    plt.savefig('./plots/heat_maps_5-shot.pdf', bbox_inches='tight')
+
+    fig, axs = plt.subplots(2, 1, sharex=True, sharey=True, figsize=(6,3))
+
+    class_labels = ["1", "2", "3", "4", "5"]
+    shot_labels = ["1"]
+
+    files = [
+        './data/scale_protonets_5-way_1-shot_epsilon-05.txt',
+        './data/scale_maml_5-way_1-shot_epsilon-05.txt'
+    ]
+
+    titles = [
+        '(a) ProtoNets, 1-shot (Clean 46.3%)',
+        '(b) MAML, 1-shot (Clean 45.9%)'
+    ]
+
+    images = []
+    for file, title, ax in zip(files, titles, [axs[0], axs[1]]):
+        data = np.genfromtxt(file, delimiter=',')
+        data = np.expand_dims(data, axis=0)
+
+        show_xticks = True
+        # if ax == axs[1]:
+        #     show_xticks = False
+
+        im = heatmap(data, shot_labels, class_labels, ax=ax, cmap="Reds", cbarlabel="% Drop in Accuracy", show_xticks=show_xticks)
+        images.append(im)
+        texts = annotate_heatmap(im, valfmt="{x:.1f}", fontsize='x-large')
+        ax.set_title(title, y=-0.45, fontsize='large', color='blue')
+        ax.set_ylabel('Shots', fontsize='large')
+
+    axs[0].xaxis.set_label_position('top')
+    axs[0].set_xlabel('Classes', fontsize='large')
+    plt.subplots_adjust(wspace=0.1)
+    fig.tight_layout()
+    plt.savefig('./plots/heat_maps_1-shot.pdf', bbox_inches='tight')
 
 
 if __name__ == '__main__':
