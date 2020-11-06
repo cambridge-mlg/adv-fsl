e97165d90f5f342bed79c1174a81cb3e6cd87bd8
cravingoxygen <vze.ezv@gmail.com>
2020-11-06 16:34:05 +0000

    Fixed indentation bug

--- learners/cnaps/src/run_cnaps.py
+++ learners/cnaps/src/run_cnaps.py
@@ -568,7 +568,7 @@ class Learner:
                                                                self.args.way, self.args.shot, self.args.query_test,
                                                                eval_images, eval_labels)
                     if self.args.continue_from_task != 0:
-                    save_partial_pickle(os.path.join(self.args.checkpoint_dir, "adv_task"), t+self.args.continue_from_task, adv_task_dict)
+                        save_partial_pickle(os.path.join(self.args.checkpoint_dir, "adv_task"), t+self.args.continue_from_task, adv_task_dict)
 
                 del adv_context_images, adv_target_images
 
